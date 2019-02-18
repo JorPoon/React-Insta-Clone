0c697d7da56b0b824a1452dad1c69fd0a156e4dc
Jor_Poon <jormpoon@gmail.com>
2019-02-18 16:37:30 -0500

    added searchbar component

---
+++ Instagram/src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+class SearchBar extends React.Component {
+    constructor() {
+        super();
+        this.state= ""
+    }
+    render() {
+        return (
+            <div className="searchNav">
+
+                <div className="Logos">
+                    <img src="" alt="Camera Logo"/>
+                    <img src="" alt="Instagram Logo"/>
+                </div>
+                
+                <input type="text" placeholder="Search"/>
+               
+                <div className="navigationIcons">
+                    <img src="" alt="location"/>
+                    <img src="" alt="heart"/>
+                    <img src="" alt="profile"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
