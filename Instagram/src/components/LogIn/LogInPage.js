8079b65d7b7be15a2f2a329184a3585d8c44fd6e
Jor_Poon <jormpoon@gmail.com>
2019-02-21 17:22:10 -0500

    added minor styling and reactstrap in log in page

--- Instagram/src/components/LogIn/LogInPage.js
+++ Instagram/src/components/LogIn/LogInPage.js
@@ -1,14 +1,30 @@
 import React from "react";
-import styled from "styled-components"
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import styled , {css} from "styled-components";
+import "./LogInPage.css";
 
 
-const logger = styled.button`
+const Putton = styled.button`
 padding: 6px 10px;
 margin: 5px;
 border: none;
 border-radius: 3px;
 color: white;
-`;
+
+${props =>
+  props.primary &&
+  css`
+    background: palevioletred;
+    color: white;
+  `};
+`
+const Liv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+`
 
 class LogInPage extends React.Component {
   state= {
@@ -28,25 +44,33 @@ class LogInPage extends React.Component {
 
   render() {
       return (
-        <div className="log-in">
-        <p>UserName
-          <input 
+
+      <div className="log-in">
+
+        <Liv inline>
+        <FormGroup>
+        <Label>UserName </Label>
+          <Input 
           type="text"
           value={this.state.username}
           onChange={this.handleChanges}
           name="username"
           />
-          </p>
-          <p>
-            Password 
-            <input 
+       
+          </FormGroup>
+          <FormGroup>
+          <Label> Password</Label>
+            <Input 
             type="text"
             value={this.state.password}
             onChange={this.handleChanges}
             name="password"
             />
-          </p>
-          <button onClick={this.logIn}>Log In</button>
+         
+          </FormGroup>
+          <Putton primary onClick={this.logIn}>Log In</Putton>
+        </Liv>
+
         </div>
       )
   }
